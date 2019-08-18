64f6c6d3f962a5813bd9d77c7122deec832841cb
amkcpu <amkcpu@outlook.com>
2019-08-18 15:37:25 -0300

    Add more comments for classifier procedure (Ej. 3)

--- tp1/e3.py
+++ tp1/e3.py
@@ -2,13 +2,17 @@
 
 # Set categories: E. g. soccer, tennis, fighting (UFC, MMA), rugby, volleyball, hockey, basketball
 
-# Select feature vectors
+# Select feature vectors (appearance of specific words)
+
+# Calculate feature vector frequency P(feature_i|class_j) = (no. of times feature_i appears in class_j) / (total count of features in class_j)
+    # remember to use Laplace (or other) smoothing
 
 # Divide data set into training and validation set
 
-# Build classifier
-
 # Classify validation data and get results
+    # Calculate feature vectore frequency
+    # Multiply P(feature_i|class_j) for all i, j, for each example
+    # Choose class with highest likelihood for each example
 
 # Construct confusion matrix
 
