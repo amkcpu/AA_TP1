9b5b307d92a92479f1a3729150206b5aec92f76d
amkcpu <amkcpu@outlook.com>
2019-08-18 14:16:40 -0300

    Groundwork for other ejercicios (2, 3, 4)

---
+++ TP1/TP1_EJ2.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+
+print("==================== Data ====================\n")
+dataset = pd.read_excel("PreferenciasBritanicos.xlsx")
+print(dataset)
+
+# Calculate P(E) and P(I)
+number_of_examples = len(dataset["Nacionalidad"])
+prob_english, prob_irish = dataset["Nacionalidad"].value_counts()
+prob_english /= number_of_examples
+prob_irish /= number_of_examples
+print("\nP(English) = ", prob_english, "\t|\tP(Irish) = ", prob_irish, "\n\n")
+
+# Loop over all attributes associated --> P(a_i|v_j) = P(a_i AND v_j)/P(v_j)
+
+
+# Calculate hypothesis for English and Scottish, respectively: Repeat-Multiply P(a_i|v_j)*P(v_j)
+
+
+# Select higher scoring hypothesis for given example (1,0,1,1,0)
---
+++ TP1/TP1_EJ3.py
@@ -0,0 +1,15 @@
+# Naive Bayes Text classifier
+
+# Set categories: E. g. soccer, tennis, fighting (UFC, MMA), rugby, volleyball, hockey, basketball
+
+# Select feature vectors
+
+# Divide data set into training and validation set
+
+# Build classifier
+
+# Classify validation data and get results
+
+# Construct confusion matrix
+
+# Calculate accuracy, precision, true positives, false positives, F1-score
