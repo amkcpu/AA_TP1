5014801df18b0d40b3603a6836395911f34c886d
Eric Horvat <eric.nahuel.horvat@gmail.com>
2019-08-19 18:23:48 -0300

    [MOD] Moving files

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Generated with pypackage cookiecutter
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+requirements = ['Click>=6.0', ]
+
+setup_requirements = [ ]
+
+test_requirements = [ ]
+
+setup(
+    author="Eric Horvat & Daniel Glatter",
+    author_email='eric.nahuel.horvat@gmail.com',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description="Machine Learning exercises from ITBA course",
+    entry_points={
+        'console_scripts': [
+            'ml_tps=ml_tps.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="GNU General Public License v3",
+    long_description= '',
+    include_package_data=True,
+    keywords='ml_tps',
+    name='ml_tps',
+    packages=find_packages(include=['ml_tps']),
+    setup_requires=setup_requirements,
+    url='https://github.com/amkcpu/Aprendizaje-Automatico---TPs',
+    version='0.1.0',
+    zip_safe=False,
+)
