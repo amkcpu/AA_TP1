014b0d717b2ea1d3699a5899e79c2b0569eb2cb4
amkcpu <amkcpu@outlook.com>
2019-08-19 21:54:19 -0300

    [WIP] Started Ej 3 (text category labels, retrieve words)

--- tp1/e3.py
+++ tp1/e3.py
@@ -1,8 +1,37 @@
 # Naive Bayes Text classifier
+import numpy as np
+import pandas as pd
+import re               # for regexp splitting words
+import codecs           # for unicode file reading
+import os               # for looping over files in directory
 
 # Set categories: E. g. soccer, tennis, fighting (UFC, MMA), rugby, volleyball, hockey, basketball
+example_labels = np.array(["soccer", "fighting", "tennis", "soccer", "soccer",
+                           "rugby", "rugby", "volleyball", "tennis", "tennis",
+                           "tennis", "hockey", "hockey", "hockey", "soccer"
+                           "soccer", "soccer", "soccer", "basketball", "fighting"])     # labels are ordered!
 
-# Select feature vectors (appearance of specific words)
+# Get words from text file into DataFrame
+path = "data/classificationDoc/sporty/"
+
+for filename in os.listdir(path):
+    text = codecs.open((path + filename), encoding="utf-8").read().lower()  # read file, ensuring that Spanish characters can be read
+    temp = pd.DataFrame(re.findall(r"[\w']+", text))            # extract words into DataFrame
+    temp.columns = [filename]                                   # assign filename as column title
+
+    try:                                                        # error handling for first round
+        words                                                   # check if words is defined
+    except NameError:
+        words = temp                                            # if not (first round), assign temp and continue to next round
+        continue
+
+    words = pd.concat([words, temp], axis=1)                    # else, concatenate existing words with new entry
+
+# Select feature vectors (appearance of specific words) or TF-IDF for generalization
+# (see https://www.freecodecamp.org/news/how-to-process-textual-data-using-tf-idf-in-python-cd2bbc0a94a3/)
+no_of_words, no_of_examples = words.shape
+for i in range(0, no_of_examples):
+    counts_in_rows = words.iloc[:, i].value_counts()
 
 # Calculate feature vector frequency P(feature_i|class_j) = (no. of times feature_i appears in class_j) / (total count of features in class_j)
     # remember to use Laplace (or other) smoothing
