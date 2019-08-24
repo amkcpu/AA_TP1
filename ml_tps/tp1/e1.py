938c8e2b2a3bbb5786ea9a59b02f496817ef6faa
Eric Horvat <eric.nahuel.horvat@gmail.com>
2019-08-23 22:40:30 -0300

    [MOD] Ej reviewed and ended

--- ml_tps/tp1/e1.py
+++ ml_tps/tp1/e1.py
@@ -21,59 +21,57 @@ def parse_probability_list(string : str):
 @click.option("--old-values", default=OLD_VALUES_DEFAULT)
 @click.option("--young-probability", default=YOUNG_PROB, type=float)
 def main(young_values, old_values, young_probability):
-    young_values = parse_probability_list(young_values)
-    old_values = parse_probability_list(old_values)
+    # Parse arguments
+    young_values = np.array(parse_probability_list(young_values))
+    old_values = np.array(parse_probability_list(old_values))
     if len(old_values) != len(young_values):
         raise ValueError("Both values list must be equals in length")
 
+    # Saves probabilities
     control_probability(young_probability)
     old_probability = 1 - young_probability
 
-    values = np.array([young_values, old_values])   # write in array
+    # Value matrix
+    values = np.array([young_values, old_values]).T   # write in array
 
-    data = pd.DataFrame(values.T,
+    data = pd.DataFrame(values,
                         range(1, len(old_values) + 1),
                         ["P(Program_i|Young)",
                          "P(Program_i|Old)"])   # note: vals are transposed
     data.index.name = "Program"    # add index column name
 
     # Add table column with probability that someone (young or old) likes the
-    # program i (= P(P_i))
-    prob_p_i = pd.DataFrame(np.array(np.zeros((len(old_values)+1, 1)))) # initialize with zeros
 
-    for i in range(0, len(prob_p_i) - 1):
-        prob_p_i.iloc[i + 1] = (data.iloc[i][0]) * young_probability + \
-                               (data.iloc[i][1]) * old_probability
-        # P(P_i) = P(P_i|J)*P(J) + P(P_i|V)*P(V)
-        # Eric comment => data*[young_probability, old_probability] ??
+    # P(P_i) = P(P_i|Y)*P(Y) + P(P_i|O)*P(O)
+    # P(P_i) = [P(P_i|Y),P(P_i|O)].[P(Y),P(O)]
+    program_probability = np.dot(values, np.array([young_probability, old_probability]))
 
-    data.insert(2, "P(Programa_i)", prob_p_i)   # append to dataset
+    data.insert(2, "P(Program_i)", program_probability)   # append to dataset
 
-    # Add tables columns for P(joven|P_i) and P(viejo|P_i)
-    prob_young_p_i = pd.DataFrame(np.array(np.zeros((len(old_values)+1, 1))))  # initialize with 0
-    prob_old_p_i = pd.DataFrame(np.array(np.zeros((len(old_values)+1, 1))))    # initialize with 0
+    # P(Y|P_i) = (P(P_i|Y) * P(Y))/P(P_i)
+    p_young_as_p_i = (young_values * young_probability) / program_probability
+    # P(O|P_i) = (P(P_i|O)*P(O))/P(P_i)
+    p_old_as_p_i = (old_values * old_probability) / program_probability
 
-    for i in range(0, len(prob_young_p_i) - 1):
-        prob_young_p_i.iloc[i + 1] = ((data.iloc[i][0]) * young_probability) / data.iloc[i][2]
-        # P(J|P_i) = (P(P_i|J)*P(J))/P(P_i)
-        prob_old_p_i.iloc[i + 1] = ((data.iloc[i][1]) * old_probability) / data.iloc[i][2]
-        # P(V|P_i) = (P(P_i|V)*P(V))/P(P_i)
-    # Eric comment => ((data.iloc[i][0]) * young_probability) => (Sub set * constant) * array (1/value saved)
+    data.insert(3, "P(Young|Program_i)", p_young_as_p_i)   # append to dataset
+    data.insert(4, "P(Old|Program_i)", p_old_as_p_i)   # append to dataset
 
-    data.insert(3, "P(Joven|Programa_i)", prob_young_p_i)   # append to dataset
-    data.insert(4, "P(Viejo|Programa_i)", prob_old_p_i)   # append to dataset
-
-    # Compute prob for given example (hypothesis) TODO check if this hypothesis computation is correct
-    example_prob_young = data.iloc[0][3]*data.iloc[0][2] + data.iloc[2][3]*data.iloc[2][2]
-    # P(J) = P(J|P1)*P(P1) + P(J|P3)*P(P3)
-    example_prob_old = data.iloc[0][4]*data.iloc[0][2] + data.iloc[2][4]*data.iloc[2][2]
-    # P(V) = P(V|P1)*P(P1) + P(V|P3)*P(P3)
+    # Compute prob for given example (hypothesis)
+    # A program likeness is independent from de others, so
+    # P(Young | 1 ∩ 0 ∩ 1 ∩ 0) = P(Y|p1) * P(Y|!p2) * P(Y|p3) * P(Y|!p4)
+    p_y_likeness = p_young_as_p_i[0] * (1 - p_young_as_p_i[1]) * p_young_as_p_i[2] * (1-p_young_as_p_i[3])
+    # P(Old | 1 ∩ 0 ∩ 1 ∩ 0) = P(O|p1) * P(O|!p2) * P(O|p3) * P(O|!p4)
+    p_o_likeness = p_old_as_p_i[0] * (1 - p_old_as_p_i[1]) * p_old_as_p_i[2] * (1-p_old_as_p_i[3])
 
     # Final printout
-    print("==================== Datos ====================")
-    print("P(Joven) = ", young_probability, " | P(Viejo) = ", old_probability, "\n")
+    print("==================== Data ====================")
+    print("P(Young) = ", young_probability, " | P(Old) = ", old_probability, "\n")
     print(data)
 
-    print("\n==================== Resultados ====================")
-    print("h_MAP(P(Joven|1,0,1,0)) = ", example_prob_young)
-    print("h_MAP(P(Viejo|1,0,1,0)) = ", example_prob_old)
+    print("================== Results ===================")
+    print("P(Young|1,0,1,0) = ", p_y_likeness)
+    print("P(Old|1,0,1,0) = ", p_o_likeness)
+
+
+if __name__ == '__main__':
+    main()
