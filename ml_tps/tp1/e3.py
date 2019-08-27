00076bbef457eee0a2d55c91b80ec60013937bd5
amkcpu <amkcpu@outlook.com>
2019-08-27 18:18:04 -0300

    [ADD] First work on evaluation metrics

--- ml_tps/tp1/e3.py
+++ ml_tps/tp1/e3.py
@@ -5,10 +5,12 @@ import pandas as pd
 import os  # for looping over files in directory
 import datetime  # measure runtime
 from ml_tps.utils.text_analysis_utils import tf_idf, extract_words_from_text
+from ml_tps.utils.evaluation_utils import f1_score
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 DATA_FILEPATH_DEFAULT = f"{dir_path}/data/aa_bayes.tsv"
-TRAINING_PERCENTAGE_DEFAULT = 0.005
+TRAINING_PERCENTAGE_DEFAULT = 0.002
+
 
 @click.command("e1_3")
 @click.option("--data-filepath", default=DATA_FILEPATH_DEFAULT)
@@ -28,35 +30,36 @@ def main(data_filepath, training_percentage, training_amount, keyword_amount):
 
     # Extract information and save in DataFrame
     data_set = pd.read_csv(path, sep="\t")
-    data_set = data_set[data_set["categoria"]!="Noticias destacadas"]   # Leave out massive, unspecific "Noticias destacadas" category
+    data_set = data_set[
+        data_set["categoria"] != "Noticias destacadas"]  # Leave out massive, unspecific "Noticias destacadas" category
 
     # More variables
     # no_of_training_examples = training_percentage * len(data_set)
-    no_of_keywords = keyword_amount     # how many highest scoring words on TF-IDF are selected as features
+    no_of_keywords = keyword_amount  # how many highest scoring words on TF-IDF are selected as features
     no_of_validation_examples = 20
 
     # TODO find smarter way to separate given data sets
     # data_set_dest = data_set[data_set["categoria"] == "Noticias destacadas"]  # Leave out massive, unspecific "Noticias destacadas" category
-    data_set_deportes = data_set[data_set["categoria"]=="Deportes"]
-    data_set_destacadas = data_set[data_set["categoria"]=="Destacadas"]
-    data_set_economia = data_set[data_set["categoria"]=="Economia"]
-    data_set_entretenimiento = data_set[data_set["categoria"]=="Entretenimiento"]
-    data_set_internacional = data_set[data_set["categoria"]=="Internacional"]
-    data_set_nacional = data_set[data_set["categoria"]=="Nacional"]
-    data_set_salud = data_set[data_set["categoria"]=="Salud"]
-    data_set_ciencia_tecnologia = data_set[data_set["categoria"]=="Ciencia y Tecnologia"]
+    data_set_deportes = data_set[data_set["categoria"] == "Deportes"]
+    data_set_destacadas = data_set[data_set["categoria"] == "Destacadas"]
+    data_set_economia = data_set[data_set["categoria"] == "Economia"]
+    data_set_entretenimiento = data_set[data_set["categoria"] == "Entretenimiento"]
+    data_set_internacional = data_set[data_set["categoria"] == "Internacional"]
+    data_set_nacional = data_set[data_set["categoria"] == "Nacional"]
+    data_set_salud = data_set[data_set["categoria"] == "Salud"]
+    data_set_ciencia_tecnologia = data_set[data_set["categoria"] == "Ciencia y Tecnologia"]
 
     categories = {"Deporte": data_set_deportes, "Destacadas": data_set_destacadas, "Economia": data_set_economia,
                   "Entretenimiento": data_set_entretenimiento, "Internacional": data_set_internacional,
                   "Nacional": data_set_nacional, "Salud": data_set_salud,
                   "CienciaTec": data_set_ciencia_tecnologia,
-                  #"NoticiasDest": data_set_dest
+                  # "NoticiasDest": data_set_dest
                   }
 
-    # Get words
+    # ============== Get words ==============
     # TODO Consider implementing Porter stemming to reduce redundancy
     #  http://www.3engine.net/wp/2015/02/stemming-con-python/
-    words_then = datetime.datetime.now()    # for measuring runtime
+    words_then = datetime.datetime.now()  # for measuring runtime
 
     words = list()  # will contain words from all data subsets, each as one list element
 
@@ -73,13 +76,14 @@ def main(data_filepath, training_percentage, training_amount, keyword_amount):
     words_now = datetime.datetime.now()
     print("Runtime of word parsing: ", divmod((words_now - words_then).total_seconds(), 60), "\n")
 
-    # Get TF-IDF TODO only get TF-IDF score for training set to reduce runtime
+    # ============== Get TF-IDF ==============
+    # TODO only get TF-IDF score for training set to reduce runtime
     then = datetime.datetime.now()  # perf measurement
 
     tf_idf_scores = list()
 
     for i in range(0, len(categories)):
-        tfidf_result = tf_idf(words[i])     # word frequencies for Bayes classifier, contains NaN
+        tfidf_result = tf_idf(words[i])  # word frequencies for Bayes classifier, contains NaN
         tf_idf_scores.append(tfidf_result)
 
     now = datetime.datetime.now()
@@ -106,20 +110,21 @@ def main(data_filepath, training_percentage, training_amount, keyword_amount):
         current_category_word_count = pd.DataFrame()
 
         for j in range(0, training_amount or int(len(cat_dataset) * training_percentage)):
-            counts_one_example = words[i].iloc[:, j].value_counts() # get word count in one example (column)
-            current_category_word_count = pd.concat([current_category_word_count, counts_one_example], axis=1, sort=True)
+            counts_one_example = words[i].iloc[:, j].value_counts()  # get word count in one example (column)
+            current_category_word_count = pd.concat([current_category_word_count, counts_one_example], axis=1,
+                                                    sort=True)
 
         category_no_of_words = current_category_word_count.sum().sum()  # get overall number of words in this category
-        temp = current_category_word_count.sum(axis=1) / category_no_of_words   # frequency of words in category
-        temp = temp[temp.index.isin(keywords[i].index)]        # choose subset of words as keywords selected above
+        temp = current_category_word_count.sum(axis=1) / category_no_of_words  # frequency of words in category
+        temp = temp[temp.index.isin(keywords[i].index)]  # choose subset of words as keywords selected above
         keyword_frequency.append(temp)
 
-    # Bayes classifier
+    # ============== Bayes classifier ==============
     validation_examples = data_set.sample(n=no_of_validation_examples)  # random sample from data set
     validation_example_predictions = list()
 
-    for i in range(0, no_of_validation_examples):       # get one example at a time
-        example_words = extract_words_from_text(validation_examples.iloc[i, 1], True)     # get words for given example
+    for i in range(0, no_of_validation_examples):  # get one example at a time
+        example_words = extract_words_from_text(validation_examples.iloc[i, 1], True)  # get words for given example
         category_wise_prob = list()
 
         for j in range(0, len(categories)):
@@ -127,28 +132,55 @@ def main(data_filepath, training_percentage, training_amount, keyword_amount):
             prob_keyword_in_entire_dataset = 0
 
             for word in example_words.iterrows():
-                try:        # TODO maybe it's necessary to smoothen the results here (Laplace smoothing)
+                try:  # TODO maybe it's necessary to smoothen the results here (Laplace smoothing)
                     prob_keyword_in_category = keywords[j][word[1].iat[0]]
-                except KeyError:    # when word not found in list of trained keywords
+                except KeyError:  # when word not found in list of trained keywords
                     continue
 
-                for k in range(0, len(categories)):     # get entire data set probability for this word
+                for k in range(0, len(categories)):  # get entire data set probability for this word
                     try:
-                        prob_keyword_in_entire_dataset += keywords[k][word[1].iat[0]] * (1/8)    # P(P_i) = P(P_i|cat1)*P(cat1) + P(P_i|cat2)*P(cat2) + ...
+                        prob_keyword_in_entire_dataset += keywords[k][word[1].iat[0]] * (
+                                    1 / 8)  # P(P_i) = P(P_i|cat1)*P(cat1) + P(P_i|cat2)*P(cat2) + ...
                     except KeyError:
                         continue
 
-                prob_this_category += prob_keyword_in_category * prob_keyword_in_entire_dataset # P(Cat) = P(Cat|Key1)*P(Key1) + P(Cat|Key2)*P(Key2) + ...
+                prob_this_category += prob_keyword_in_category * prob_keyword_in_entire_dataset  # P(Cat) = P(Cat|Key1)*P(Key1) + P(Cat|Key2)*P(Key2) + ...
             category_wise_prob.append(prob_this_category)
 
-        predicted_class = category_wise_prob.index(max(category_wise_prob))     # find class with highest probability
-        predicted_class_name = list(categories.keys())[predicted_class]         # find associated class name
+        predicted_class = category_wise_prob.index(max(category_wise_prob))  # find class with highest probability
+        predicted_class_name = list(categories.keys())[predicted_class]  # find associated class name
         validation_example_predictions.append(predicted_class_name)
 
-    # evaluation
-        # confusion matrix
-        # TP rate, FP rate
-        # accuracy, precision, recall, F1-score
+    validation_example_predictions_wrapper = pd.Series(validation_example_predictions)
+
+    # ============== evaluation ==============
+    # confusion matrix
+    validation_examples_actual_category = validation_examples["categoria"]
+    validation_examples_actual_category.index = validation_example_predictions_wrapper.index
+    confusion_matrix = pd.crosstab(validation_examples_actual_category, validation_example_predictions_wrapper,
+                                   rownames=['Actual'], colnames=['Predicted'],
+                                   margins=True)
+
+
+    # TP rate, FP rate
+    # accuracy, precision, recall, F1-score
+    precision = 0
+    recall = 0
+    accuracy = 0
+    f1 = f1_score(precision, recall)
+
+    # ============== Final printout ==============
+    print("\n========== Data set info ==========")
+    print("Data set dimensions:", data_set.shape)
+    print("Category info:", categories.keys())
+
+    print("\n========== Classifier info ==========")
+    print("Number of training examples: ", current_category_word_count.shape[0])
+    print("Number of validation examples: ", no_of_validation_examples)
+
+    print("\n========== Evaluation metrics ==========")
+    print("Accuracy: ", accuracy)
+    print("Confusion matrix:")
 
     a = 1
     # ============ Old instructions ==============
@@ -161,5 +193,6 @@ def main(data_filepath, training_percentage, training_amount, keyword_amount):
     # Calculate feature vector frequency
     # Multiply P(feature_i|class_j) for all i, j, for each example
 
-if __name__ ==  "__main__":
+
+if __name__ == "__main__":
     main()
