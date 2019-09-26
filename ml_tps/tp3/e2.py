4b78945e253111c1bf6d619899c48633784baba2
amkcpu <amkcpu@outlook.com>
2019-09-26 15:15:34 -0300

    [ADD] E2c) parameter and kernel testing

--- ml_tps/tp3/e2.py
+++ ml_tps/tp3/e2.py
@@ -3,6 +3,7 @@ import datetime
 
 from ml_tps.utils.dataframe_utils import divide_in_training_test_datasets
 import pandas as pd
+import numpy as np
 import os
 from sklearn import svm
 from sklearn.preprocessing import MinMaxScaler
@@ -34,25 +35,33 @@ def main():
 
     words_then = datetime.datetime.now()
     # b)  Classify categorical variable "sigdz" using default SVC SVM
-    classifiers = list()
-    accuracies = list()
-    classifiers.append(svm.SVC(kernel='rbf', gamma='auto', C=1))        # using default parameters, written down for illustrative purposes
-    classifiers[0].fit(X_train, y_train)
-    predictions_test = pd.Series(classifiers[0].predict(X_test).T)
+    svm_values = pd.DataFrame(columns=["Kernel", "C value", "Accuracy"])
+    c_value1 = 1
+    kernel1 = "rbf"
+    clf = svm.SVC(kernel=kernel1, gamma='scale', C=c_value1)      # using default parameters, written down for illustrative purposes
+    clf.fit(X_train, y_train)
+    predictions_test = pd.Series(clf.predict(X_test).T)
     confusion_matrix = getConfusionMatrix(predictions_test, y_test)
-    accuracies.append(computeAccuracy(predictions_test, y_test))
+    accuracy1 = computeAccuracy(predictions_test, y_test)
+
+    svm_values.loc[0] = [kernel1, c_value1, accuracy1]
 
     words_now = datetime.datetime.now()
-    print("Runtime SVM1: ", divmod((words_now - words_then).total_seconds(), 60), "\n")
+    print("Runtime Default SVM fitting and testing: ", divmod((words_now - words_then).total_seconds(), 60), "\n")
 
     # c)  Evaluate different values for C and different nuclei to find better performing classifiers
-    clf = svm.SVC(kernel='poly', degree=3, C=1, cache_size=500)
-    clf.fit(X_train, y_train)
-    predictions_test2 = pd.Series(clf.predict(X_test).T)
-    confusion_matrix2 = getConfusionMatrix(predictions_test2, y_test)
+    for kernel in ["rbf", "poly", "linear", "sigmoid"]:
+        for c_value in np.logspace(-3, 2, 6):   # TODO SVM with C=0.001 seem to predict only 1's -> error in computeAccuracy
+            clf = svm.SVC(kernel=kernel, C=c_value, gamma="scale", cache_size=500)
+            print(clf)
+            clf.fit(X_train, y_train)
+            predictions = pd.Series(clf.predict(X_test).T)
+            accuracy = computeAccuracy(predictions, y_test)
+
+            svm_values.loc[svm_values.index.max() + 1] = [kernel, c_value, accuracy]
 
     time_now = datetime.datetime.now()
-    print("Runtime SVM2: ", divmod((time_now - words_now).total_seconds(), 60), "\n")
+    print("\n\nRuntime C Parameter testing: ", divmod((time_now - words_now).total_seconds(), 60), "\n")
 
     a = 1
 
