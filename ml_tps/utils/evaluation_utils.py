0044c49fe708c2390a1a608f59dc52c5b9bc01e5
amkcpu <amkcpu@outlook.com>
2019-09-27 07:36:05 -0300

    [MOD] Improve eval_utils

--- ml_tps/utils/evaluation_utils.py
+++ ml_tps/utils/evaluation_utils.py
@@ -1,45 +1,18 @@
 import pandas as pd
-import numpy as np
 
 
-# TODO revise if this is necessary with new confusion_matrix_diag() in place
-def evalPreprocessing(predictions: pd.Series,
-                      actual: pd.Series,
-                      prediction_labels: list = None,
-                      actual_labels: list = None):
-    pred_saver = predictions
-    ac_saver = actual
-    if prediction_labels is not None:
-        temp_pred = pd.Series()
-        for index in predictions.index:
-            if predictions[index] in prediction_labels:
-                temp_pred = temp_pred.append(pd.Series([predictions[index]], index=[index]))
-        predictions = temp_pred
-        actual = actual[predictions.index]
-    if actual_labels is not None:
-        temp_actual = pd.Series()
-        for index in actual.index:
-            if actual[index] in actual_labels:
-                temp_actual = temp_actual.append(pd.Series([predictions[index]], index=[index]))
-        actual = temp_actual
-        predictions = predictions[actual.index]
-
+def evalPreprocessing(predictions: pd.Series, actual: pd.Series):
     if len(predictions) != len(actual):
         raise ValueError("Number of predictions does not equal number of validation examples (actual).")
 
-    predicted_labels = predictions.value_counts().index
-    actually_used_labels = actual.value_counts().index
-    if len(predicted_labels) != len(actually_used_labels):
-        raise ValueError("Number of predicted labels does not equal number of actual labels.")
+    predictions.index = actual.index
 
     return predictions, actual
 
 
 def getConfusionMatrix(predictions: pd.Series, actual: pd.Series):
-    if len(predictions) != len(actual):
-        raise ValueError("Mismatch between number of predictions and number of actual objective values.")
+    predictions, actual = evalPreprocessing(predictions, actual)
 
-    predictions.index = actual.index        # so indices are equal
     return pd.crosstab(index=predictions,
                        columns=actual,
                        rownames=['Predicted'],
@@ -47,16 +20,21 @@ def getConfusionMatrix(predictions: pd.Series, actual: pd.Series):
                        dropna=False)
 
 
-# Expects true objective values to be the columns
-def getConfusionMatrixDiag(confusion_matrix: pd.DataFrame):
-    cm_diag = {}
-    for col in confusion_matrix.columns:
+def getConfusionMatrixDiag(confusion_matrix: pd.DataFrame, actual_classes_in_index: bool = False):
+    confusion_matrix_diag = {}
+
+    if actual_classes_in_index:
+        actual_classes = confusion_matrix.index
+    else:
+        actual_classes = confusion_matrix.columns
+
+    for elem in actual_classes:
         try:
-            cm_diag[col] = confusion_matrix.loc[col, col]
+            confusion_matrix_diag[elem] = confusion_matrix.loc[elem, elem]
         except KeyError:
             continue
 
-    return pd.Series(cm_diag)
+    return pd.Series(confusion_matrix_diag)
 
 
 def computeAccuracy(predictions: pd.Series,
@@ -70,20 +48,15 @@ def computeAccuracy(predictions: pd.Series,
 
 # TODO Fix TN and assure correctness in multi-class applications
 # Returns parameters as vectors with entry for each objective class
-def getEvaluationParameters(predictions: pd.Series,
-                    actual: pd.Series,
-                    prediction_labels: list = None,
-                    actual_labels: list = None):
-    predictions, actual = evalPreprocessing(predictions, actual,
-                                            prediction_labels=prediction_labels,
-                                            actual_labels=actual_labels)
+def getEvaluationParameters(predictions: pd.Series, actual: pd.Series):
+    predictions, actual = evalPreprocessing(predictions, actual)
     confusion_matrix = getConfusionMatrix(predictions, actual)
     confusion_matrix_diag = getConfusionMatrixDiag(confusion_matrix)
 
     FP = confusion_matrix.sum(axis=1) - confusion_matrix_diag
     FN = confusion_matrix.sum(axis=0) - confusion_matrix_diag
     TP = confusion_matrix_diag
-    # TN for each class is the sum of all values of the confusion matrix excluding that class's row and column
+    # TN for each class is the sum of all values of the confusion matrix excluding that class's row and column (?)
     TN = confusion_matrix.sum().sum() - confusion_matrix.sum(axis=0) - confusion_matrix.sum(axis=1)
     # TN = confusion_matrix.sum() - (FP + FN + TP)
 
@@ -97,25 +70,14 @@ def getEvaluationParameters(predictions: pd.Series,
     return FP, FN, TP, TN
 
 
-def computeRecall(predictions: pd.Series,
-                    actual: pd.Series,
-                    prediction_labels: list = None,
-                    actual_labels: list = None):
-    predictions, actual = evalPreprocessing(predictions, actual,
-                                            prediction_labels=prediction_labels,
-                                            actual_labels=actual_labels)
+def computeRecall(predictions: pd.Series, actual: pd.Series):
     return computeTruePositiveRate(predictions, actual)
 
 
+# TODO what if confusion matrix not squared
 # TPR = sum(True positives)/sum(Condition positive)
-def computeTruePositiveRate(predictions: pd.Series,
-                    actual: pd.Series,
-                    averaged: bool = True,
-                    prediction_labels: list = None,
-                    actual_labels: list = None):
-    predictions, actual = evalPreprocessing(predictions, actual,
-                                            prediction_labels=prediction_labels,
-                                            actual_labels=actual_labels)
+def computeTruePositiveRate(predictions: pd.Series, actual: pd.Series, averaged: bool = True):
+    predictions, actual = evalPreprocessing(predictions, actual)
     confusion_matrix = getConfusionMatrix(predictions, actual)
     confusion_matrix_diag = getConfusionMatrixDiag(confusion_matrix)
 
@@ -129,12 +91,8 @@ def computeTruePositiveRate(predictions: pd.Series,
 
 # FPR = sum(False positives)/sum(Condition negative)
 # TODO Fix computation of False Positive Rate
-def computeFalsePositiveRate(predictions: pd.Series, actual: pd.Series, averaged: bool = True,
-                             prediction_labels: list = None,
-                             actual_labels: list = None):
-    predictions, actual = evalPreprocessing(predictions, actual,
-                                            prediction_labels=prediction_labels,
-                                            actual_labels=actual_labels)
+def computeFalsePositiveRate(predictions: pd.Series, actual: pd.Series, averaged: bool = True):
+    predictions, actual = evalPreprocessing(predictions, actual)
     confusion_matrix = getConfusionMatrix(predictions, actual)
     confusion_matrix_diag = getConfusionMatrixDiag(confusion_matrix)
 
@@ -155,12 +113,9 @@ def computeFalsePositiveRate(predictions: pd.Series, actual: pd.Series, averaged
     return FP / condition_negative
 
 
-def computePrecision(predictions: pd.Series, actual: pd.Series, averaged: bool = True,
-                     prediction_labels: list = None,
-                     actual_labels: list = None):
-    predictions, actual = evalPreprocessing(predictions, actual,
-                                            prediction_labels=prediction_labels,
-                                            actual_labels=actual_labels)
+# TODO what if confusion matrix not squared
+def computePrecision(predictions: pd.Series, actual: pd.Series, averaged: bool = True):
+    predictions, actual = evalPreprocessing(predictions, actual)
     confusion_matrix = getConfusionMatrix(predictions, actual)
     confusion_matrix_diag = getConfusionMatrixDiag(confusion_matrix)
 
