559e5133651926f380c6856417399f8a8b29191d
amkcpu <amkcpu@outlook.com>
2019-10-03 19:48:43 -0300

    [ADD] Finish e3 and jupyter notebook

--- ml_tps/tp3/e3.py
+++ ml_tps/tp3/e3.py
@@ -5,15 +5,15 @@ import datetime
 import pandas as pd
 import numpy as np
 import os
-from sklearn import svm
-from ml_tps.utils.dataframe_utils import divide_in_training_test_datasets, scale_dataset, \
-    seperateDatasetObjectiveData, get_test_train_X_y
+from ml_tps.utils.dataframe_utils import divide_in_training_test_datasets, scale_dataset, get_test_train_X_y
 import matplotlib.pyplot as plt
-from ml_tps.utils.image_utils import read_image_to_dataframe
+from ml_tps.utils.image_utils import read_image_to_dataframe, segment_image, dataframe_to_rgb, segment_and_draw
 from ml_tps.tp3.svm_utils import test_svm_configurations
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 DEFAULT_FILEPATH = f"{dir_path}/../tp3/data/"
+PCTG_DATA_TO_USE = 0.1      # so that computation does not take too long, use smaller percentage of data
+
 
 def main():
     filepath_sky = DEFAULT_FILEPATH + "cielo.jpg"
@@ -22,69 +22,74 @@ def main():
     filepath_given_test_image = DEFAULT_FILEPATH + "cow.jpg"
     filepath_own_test_image = DEFAULT_FILEPATH + "field_sky_test_image.jpg"
 
-    objective_sky = "Sky"
-    objective_cow = "Cow"
-    objective_grass = "Grass"
+    sky_description = "Sky"
+    cow_description = "Cow"
+    grass_description = "Grass"
+    objective_desc = "Objective"
 
     # a)  Construir un conjunto de datos para entrenamiento, indicando para cada muestra a qué clase pertenece.
-    data_sky = read_image_to_dataframe(filepath_sky)
-    data_cow = read_image_to_dataframe(filepath_cow)
-    data_grass = read_image_to_dataframe(filepath_grass)
+    data_sky, discard_sky = divide_in_training_test_datasets(read_image_to_dataframe(filepath_sky), PCTG_DATA_TO_USE)
+    data_cow, discard_cow = divide_in_training_test_datasets(read_image_to_dataframe(filepath_cow), PCTG_DATA_TO_USE)
+    data_grass, discard_grass = divide_in_training_test_datasets(read_image_to_dataframe(filepath_grass), PCTG_DATA_TO_USE)
     data_given_test_image = read_image_to_dataframe(filepath_given_test_image)
     data_own_test_image = read_image_to_dataframe(filepath_own_test_image)
 
     # Add objective columns
-    data_sky["Objective"] = objective_sky
-    data_cow["Objective"] = objective_cow
-    data_grass["Objective"] = objective_grass
+    data_sky[objective_desc] = sky_description
+    data_cow[objective_desc] = cow_description
+    data_grass[objective_desc] = grass_description
 
     # Scale image data
-    data_sky = scale_dataset(dataset=data_sky, objective="Objective")
-    data_cow = scale_dataset(dataset=data_cow, objective="Objective")
-    data_grass = scale_dataset(dataset=data_grass, objective="Objective")
+    data_sky = scale_dataset(dataset=data_sky, objective=objective_desc)
+    data_cow = scale_dataset(dataset=data_cow, objective=objective_desc)
+    data_grass = scale_dataset(dataset=data_grass, objective=objective_desc)
     data_given_test_image = scale_dataset(dataset=data_given_test_image, objective=None)
     data_own_test_image = scale_dataset(dataset=data_own_test_image, objective=None)
 
-    # TODO Merge data for classification
-    merged_data = 1
+    # Merge data for classification
+    merged_data = pd.concat([data_sky, data_cow, data_grass], ignore_index=True)
 
     # b)  Divide data set into training and cross-validation (cv) set
-    X_train, y_train, X_cv_set, y_cv_set = get_test_train_X_y(merged_data, objective=objective_sky)
+    X_train, y_train, X_cv_set, y_cv_set = get_test_train_X_y(merged_data, objective=objective_desc)
 
     # c)  Utilizar el método SVM para clasificar los pixels del conjunto de prueba, entrenando con el conjunto de entrenamiento.
     kernels = ["rbf", "poly", "linear", "sigmoid"]
     c_values = list(np.logspace(-3, 2, 6))
 
-    # TODO test_svm_configurations() for each example
+    # test_svm_configurations() for classification + Confusion Matrix for each one
     svm_values, best_svm = test_svm_configurations(kernels=kernels, c_values=c_values, X_train=X_train, y_train=y_train,
-                                                   X_cv_set=X_cv_set, y_cv_set=y_cv_set, alwaysPrintConfusionMatrix=True)
-
-    # Confusion Matrix for each one (edit test_svm_configs()?)
+                                                   X_cv_set=X_cv_set, y_cv_set=y_cv_set, printConfusionMatrices=True)
+    print(svm_values)
 
     # d)  ¿Cuál es el núcleo que da mejores resultados? Pensar una justificación teórica para la respuesta.
-    # TODO Average over each kernel's accuracy and get best
+    # Average over each kernel's accuracy and get best result
+    kernels_average = pd.Series()
+    i = 0
+    for kernel in kernels:
+        kernels_average[kernel] = svm_values[svm_values["Kernel"] == kernel]["CV set accuracy"].mean()
+        i += 1
+
+    best_kernel = kernels_average.sort_values(ascending=False).head(1)
+    print(best_kernel)
 
     # e)  Con el mismo método ya entrenado clasificar todos los pixels de la imagen.
-    # TODO classify all pixels in raw_given_test_image
+    best_svm.fit(X_train, y_train)
 
-    # TODO Put all of this in method segment_image()
-    # TODO Translate classification to color (3 columns representing RGB)
-    #  -> Get relevant indices: predicts[predicts == "sky"].index etc.
-    #  -> make DataFrame with np.zeros([len(predicts), 3])
-    #  -> for indices fill respective color in column
-    #  -> Pasto = Green: 0,255,0 | Vaca = Red: 255,0,0 | Cielo = Blue: 0,0,255
-    # TODO convert DataFrame to nparray to be able to pass it to imshow()
-    #  -> convert_dataframe_to_image()
-    #  -> df.to_numpy()
-    # TODO Reshape array
-    #  -> predicts.reshape(raw_given_test_image.shape)
-    # TODO Draw image
-    #  -> plt.imshow() + plt.show()
+    red = [255, 0, 0]
+    green = [0, 255, 0]
+    blue = [0, 0, 255]
+    colors = [blue, red, green]
+    classes = [sky_description, cow_description, grass_description]
 
-    # f)  Con el mismo método ya entrenado clasificar todos los pixels de otra imagen.
-    # TODO Same as in e) with raw_own_test_image
+    segment_and_draw(fitted_classifier=best_svm, image=data_given_test_image,
+                     classes=classes, rgb_colors=colors, height=760, width=1140)
+
+    # f)  Con el mismo método ya entrenado clasificar todos los pixels de otra imagen
+    segment_and_draw(fitted_classifier=best_svm, image=data_own_test_image,
+                     classes=classes, rgb_colors=colors, height=627, width=1200)
 
     a = 1
+
 
 if __name__ == '__main__':
     main()
--- ml_tps/utils/image_utils.py
+++ ml_tps/utils/image_utils.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import numpy as np
 import matplotlib.image as img
+import matplotlib.pyplot as plt
 
 
 def read_image_to_dataframe(filepath: str):
@@ -12,3 +14,33 @@ def rgb_to_dataframe(raw_image_data):
     dataframe = pd.DataFrame(dataframe)
 
     return dataframe
+
+
+def dataframe_to_rgb(image_data: pd.DataFrame, height: int, width: int):
+    rgb_image = image_data.to_numpy()
+    rgb_image = rgb_image.reshape(height, width, 3)
+
+    return rgb_image
+
+
+# Segment image and give each region a different color
+def segment_image(fitted_classifier, image: pd.DataFrame, classes: list, rgb_colors: list):
+    predictions = pd.Series(fitted_classifier.predict(image))
+    segmented_image = pd.DataFrame(np.zeros([len(predictions), 3]))
+
+    i = 0
+    for cls in classes:
+        indices = predictions[predictions == cls].index.values  # Sky -> Blue (0,0,225)
+        segmented_image.loc[indices, :] = rgb_colors[i]
+        i += 1
+
+    return segmented_image
+
+
+def segment_and_draw(fitted_classifier, image: pd.DataFrame, classes: list, rgb_colors: list, height: int, width: int):
+    segmented_image = segment_image(fitted_classifier=fitted_classifier, image=image,
+                                    classes=classes, rgb_colors=rgb_colors)
+    drawable_image = dataframe_to_rgb(image_data=segmented_image, height=height, width=width)
+
+    plt.imshow(drawable_image)
+    plt.show()
