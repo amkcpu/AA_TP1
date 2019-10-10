31ea3b1ab784556d012d869552325be6da947bdc
amkcpu <amkcpu@outlook.com>
2019-10-10 15:23:35 -0300

    [ADD] Evaluation criteria and refactor linear_reg_utils

--- ml_tps/regression_exercise/exercise_cerveza.py
+++ ml_tps/regression_exercise/exercise_cerveza.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 from ml_tps.utils.dataframe_utils import get_test_train_X_y
-from ml_tps.utils.regression_utils.linear_regression import fit_using_normal_equation, plot_2d
+from ml_tps.utils.regression_utils.linear_regression import LinearRegression
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 DEFAULT_FILEPATH = f"{dir_path}/../regression_exercise/data/"
@@ -16,17 +16,23 @@ def main():
     X_train, y_train, X_test, y_test = get_test_train_X_y(data=dataset, objective=OBJECTIVE, train_pctg=TRAIN_PCTG)
 
     # Predict according to "Cajas"
-    X_train_cajas = X_train.drop("Distancia", axis=1)
-    b_cajas = fit_using_normal_equation(X=X_train_cajas, y=y_train)
-    plot_2d(X_train_cajas, y_train, b_cajas)
-
+    X_train_cajas = X_train.drop(columns=["Distancia"])
+    cajas_classifier = LinearRegression()
+    cajas_classifier.fit(X=X_train_cajas, y=y_train)
+    cajas_classifier.plot_2d(X=X_train_cajas, y=y_train)
 
     # Predict according to "Distancia"
-    X_train_distancia = X_train.drop("Cajas", axis=1)
-    b_distancia = fit_using_normal_equation(X=X_train_distancia, y=y_train)
-    plot_2d(X_train_distancia, y_train, b_distancia)
+    X_train_distancia = X_train.drop(columns=["Cajas"])
+    distancia_classifier = LinearRegression()
+    distancia_classifier.fit(X=X_train_distancia, y=y_train)
+    distancia_classifier.plot_2d(X=X_train_distancia, y=y_train)
 
-    # TODO Compare models calculating RSS
+    # Compare models calculating RSS
+    cajas_R2 = cajas_classifier.calculate_r2(X_train_cajas, y_train)
+    cajas_adj_R2 = cajas_classifier.calculate_adjusted_r2(X_train_cajas, y_train)
+
+    distancia_R2 = distancia_classifier.calculate_r2(X_train_distancia, y_train)
+    distancia_adj_R2 = distancia_classifier.calculate_adjusted_r2(X_train_distancia, y_train)
 
     a = 1
 
--- ml_tps/utils/regression_utils/linear_regression.py
+++ ml_tps/utils/regression_utils/linear_regression.py
@@ -3,32 +3,58 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def fit_using_normal_equation(X: pd.DataFrame, y: pd.Series):
-    X = add_bias_to_dataset(X)
-    return np.linalg.inv(X.T @ X) @ (X.T @ y)       # b = inv(X'X)*X'y
-
-
 def add_bias_to_dataset(dataset: pd.DataFrame):
     ones = pd.Series(np.ones(max(dataset.index) + 1))
     dataset_copy = dataset.copy()
-    dataset_copy.insert(0, "Bias", ones)         # works inplace
+    dataset_copy.insert(0, "Bias", ones)  # works inplace
 
     return dataset_copy
 
 
-def plot_2d(X: pd.DataFrame, y: pd.Series, b):
-    if len(X.columns) != 1:
-        raise ValueError("This method only plots 2D data and regression lines. "
-                         "The X that was passed has {} columns instead of 1.".format(len(X.columns)))
+class LinearRegression:
 
-    x_sampling = pd.DataFrame(np.linspace(min(X.values), max(X.values), 1000))
+    def __init__(self, initial_b: pd.Series = None):
+        if initial_b is None:
+            self.b = 0
+        else:
+            self.b = initial_b
 
-    plt.plot(X.values, y.values, "bo")
-    plt.plot(x_sampling, predict(x_sampling, b), "r")
-    plt.show()
+    def fit(self, X: pd.DataFrame, y: pd.Series):       # using Normal Equation approach
+        X = add_bias_to_dataset(X)
+        self.b = np.linalg.inv(X.T @ X) @ (X.T @ y)     # b = inv(X'X)*X'y
 
+    def predict(self, X: pd.DataFrame):
+        X = add_bias_to_dataset(X)
 
-def predict(X: pd.DataFrame, b):
-    X = add_bias_to_dataset(X)
+        return X @ self.b
 
-    return X @ b
+    def plot_2d(self, X: pd.DataFrame, y: pd.Series):
+        if len(X.columns) != 1:
+            raise ValueError("This method only plots 2D data and regression lines. "
+                             "The X that was passed has {} columns instead of 1.".format(len(X.columns)))
+
+        x_sampling = pd.DataFrame(np.linspace(min(X.values), max(X.values), 1000))
+
+        plt.plot(X.values, y.values, "bo")
+        plt.plot(x_sampling, self.predict(x_sampling), "r")
+        plt.show()
+
+    def calculate_sums_of_squares(self, X: pd.DataFrame, y: pd.Series):
+        TSS = (y - y.mean()).T @ (y - y.mean())  # TSS = (y-mean(y)'(y-mean(y)
+        errors = y - self.predict(X)  # e = y - Xb
+        RSS = errors.T @ errors  # RSS = e'e
+        ESS = TSS - RSS  # ESS = TSS - RSS
+
+        return TSS, ESS, RSS
+
+    def calculate_r2(self, X: pd.DataFrame, y: pd.Series):
+        TSS, ESS, RSS = self.calculate_sums_of_squares(X, y)
+
+        return ESS / TSS
+
+    def calculate_adjusted_r2(self, X: pd.DataFrame, y: pd.Series):
+        R2 = self.calculate_r2(X, y)
+        no_examples = len(X.index)
+        no_predictors = len(X.columns)
+
+        return 1 - (1 - R2) * ((no_examples - 1) / (no_examples - no_predictors - 1))
