e8919c5da8bbf8f4c04dbbf12a53e6482345b304
amkcpu <amkcpu@outlook.com>
2019-10-17 15:45:11 -0300

    [MOD] Improve KNN performance

--- ml_tps/tp2/ej3.py
+++ ml_tps/tp2/ej3.py
@@ -1,55 +1,49 @@
 # Trabajo practico 2 - Ejercicio 3
 
+import pandas as pd
 from ml_tps.utils.k_nearest_neighbors import knn
 from ml_tps.utils.evaluation_utils import *
 from ml_tps.utils.dataframe_utils import divide_in_training_test_datasets, \
-    rewrite_positives_negatives, delete_non_numeric_columns
+    rewrite_positives_negatives, delete_non_numeric_columns, separate_dataset_objective_data
+import datetime
 
 DEFAULT_FILEPATH = "data/reviews_sentiment.csv"
 DEFAULT_K = 5
-DEFAULT_TRAINING_SET_SIZE_PCTG = 0.6
+TRAIN_PCTG = 0.6
 DEFAULT_OBJECTIVE = "Star Rating"
 
 
 def main():
+    # Data import and cleaning
     dataset = pd.read_csv(DEFAULT_FILEPATH, sep=';')  # review_sentiments.csv is semicolon-separated (;)
     dataset = rewrite_positives_negatives(dataset)
     dataset = delete_non_numeric_columns(dataset)
-
-    # Data set specific changes
-    dataset["titleSentiment"] = dataset["titleSentiment"].fillna(dataset["textSentiment"])
+    dataset["titleSentiment"] = dataset["titleSentiment"].fillna(dataset["textSentiment"])  # Handle NaN
 
     # ========== a) Mean no. of words of reviews valued with 1 star
     one_star_ratings = dataset[dataset["Star Rating"] == 1]
     one_star_review_mean_words = sum(one_star_ratings["wordcount"]) / len(one_star_ratings)
 
     # ========== b) Divide data set into two parts, training and evaluation set
-    train_pctg = DEFAULT_TRAINING_SET_SIZE_PCTG
-    training_set, evaluation_set = divide_in_training_test_datasets(dataset, train_pctg)
+    training_set, evaluation_set = divide_in_training_test_datasets(dataset=dataset, train_pctg=TRAIN_PCTG)
+    evaluation_set_without_objective, orig_ratings = separate_dataset_objective_data(dataset=evaluation_set,
+                                                                                     objective=DEFAULT_OBJECTIVE)
 
     # ========== c) Apply KNN and Weighted-distances KNN to predict review ratings (stars)
-    # DEFAULT_K = 5, DEFAULT_OBJECTIVE = "Star Rating"
-    evaluation_set_without_objective = evaluation_set.copy()
-    del evaluation_set_without_objective[DEFAULT_OBJECTIVE]
+    time1 = datetime.datetime.now()
+    predicted_ratings = evaluation_set_without_objective.apply(knn, axis=1,
+                                                               args=(training_set, DEFAULT_OBJECTIVE, DEFAULT_K, False))
+    time2 = datetime.datetime.now()
+    print("Runtime Unweighted: ", divmod((time2 - time1).total_seconds(), 60), "\n")
 
-    predicted_ratings = pd.Series(np.array(np.zeros(len(evaluation_set.index), dtype=int)))
-    i = 0
-    for index, example in evaluation_set_without_objective.iterrows():
-        predicted_ratings[i] = knn(example, training_set, DEFAULT_OBJECTIVE, DEFAULT_K, False)
-        i += 1
-
-    predicted_ratings_weighted = pd.Series(np.array(np.zeros(len(evaluation_set.index), dtype=int)))
-    i = 0
-    for index, example in evaluation_set_without_objective.iterrows():
-        predicted_ratings_weighted[i] = knn(example, training_set, DEFAULT_OBJECTIVE, DEFAULT_K, True)
-        i += 1
+    predicted_ratings_weighted = evaluation_set_without_objective.apply(knn, axis=1,
+                                                                        args=(training_set, DEFAULT_OBJECTIVE, DEFAULT_K, True))
+    print("Runtime Weighted: ", divmod((datetime.datetime.now() - time2).total_seconds(), 60), "\n")
 
     # ========== d) Calculate classifier precision and confusion matrix
-    orig_ratings = evaluation_set[DEFAULT_OBJECTIVE]
     confusion_matrix = getConfusionMatrix(predicted_ratings, orig_ratings)
     accuracy = computeAccuracy(predicted_ratings, orig_ratings)
     true_positive_rate = computeTruePositiveRate(predicted_ratings, orig_ratings)
-    # false_positive_rate = computeFalsePositiveRate(predicted_ratings, orig_ratings)
     precision = computePrecision(predicted_ratings, orig_ratings)
     recall = computeRecall(predicted_ratings, orig_ratings)
     f1 = f1_score(precision, recall)
@@ -70,7 +64,7 @@ def main():
     print("Data set dimensions: ", dataset.shape)
     print("Training set dimensions: ", training_set.shape)
     print("Evaluation set dimensions: ", evaluation_set.shape)
-    print("Percentage of data set used for training: ", int(train_pctg*100), "%")
+    print("Percentage of data set used for training: ", int(TRAIN_PCTG*100), "%")
     print("Classification objective: ", DEFAULT_OBJECTIVE)
 
     print("\n========== Evaluation metrics standard KNN ==========")
@@ -79,7 +73,6 @@ def main():
     print("Confusion matrix:\n", confusion_matrix)
 
     print("\nTrue positive rate (TP) (= Recall): ", true_positive_rate)
-    # print("False positive rate (FP): ", false_positive_rate)  TODO needs fixing in evaluation_utils.py
     print("Precision: ", precision)
     print("Recall: ", recall)
     print("F1-score: ", f1)
@@ -90,7 +83,6 @@ def main():
     print("Confusion matrix:\n", confusion_matrix_weighted)
 
     print("\nTrue positive rate (TP) (= Recall): ", true_positive_rate_weighted)
-    # print("False positive rate (FP): ", false_positive_rate)  TODO needs fixing in evaluation_utils.py
     print("Precision: ", precision_weighted)
     print("Recall: ", recall_weighted)
     print("F1-score: ", f1_weighted)
--- ml_tps/utils/k_nearest_neighbors.py
+++ ml_tps/utils/k_nearest_neighbors.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from ml_tps.utils.dataframe_utils import drop_objective_column
+from ml_tps.utils.dataframe_utils import separate_dataset_objective_data
 from ml_tps.utils.distance_utils import euclidean_distance
 
 
@@ -17,37 +17,27 @@ def knn(example: pd.Series, training_set: pd.DataFrame, objective: str, k: int, 
 
 
 def get_nearest_neighbors(example: pd.Series, training_set: pd.DataFrame, objective: str, k: int):
-    training_set_values = drop_objective_column(training_set, objective)
-
-    # Transpose training set & set example columns to have correct subtraction
+    training_set_values, obj_values = separate_dataset_objective_data(dataset=training_set, objective=objective)
     example.index = training_set_values.columns
-    training_set_values = training_set_values.transpose()
 
-    # DataFrame to store calculated distances and objective value
-    distances_and_objective = pd.DataFrame(np.array(np.zeros([len(training_set_values.columns), 2])),
-                                           index=training_set_values.columns,
-                                           columns=["Distance", "Objective Value"])
-
-    # Calc distance and write objective
-    for element in training_set_values:
-        distances_and_objective["Distance"][element] = euclidean_distance(example, training_set_values[element])
-        distances_and_objective["Objective Value"][element] = training_set[objective].loc[element]
-
-    distances_and_objective = distances_and_objective.sort_values(by="Distance", ascending=True)
-    return distances_and_objective.head(k)  # grab k nearest neighbors
+    neighbors = pd.Series([euclidean_distance(row, example) for idx, row in training_set_values.iterrows()],
+                          index=training_set_values.index)
+    neighbors = pd.concat([neighbors, obj_values], axis=1)
+    neighbors.columns = ["Distance", "Class"]
+    neighbors = neighbors.sort_values(by="Distance", ascending=True)
+    return neighbors.head(k)  # grab k nearest neighbors
 
 
 def prediction_per_class(nearest_neighbors: pd.DataFrame, weighted: bool):
-    available_classes = nearest_neighbors["Objective Value"].unique()  # which classes occur in nearest neighbors
+    available_classes = nearest_neighbors["Class"].unique()  # which classes occur in nearest neighbors
     predicted_classes = pd.Series(np.array(np.zeros(len(available_classes))), index=available_classes)
 
     for this_class in available_classes:
-        class_members = nearest_neighbors[nearest_neighbors["Objective Value"] == this_class]
-        if weighted:
-            predicted_classes[this_class] = sum(
-                1 / np.square(class_members["Distance"]))  # If weighted, use 1/dist^2 as weight
+        class_members = nearest_neighbors[nearest_neighbors["Class"] == this_class]
+        if weighted:    # If weighted, use 1/dist^2 as weight
+            predicted_classes[this_class] = sum(1 / np.square(class_members["Distance"]))
         else:
-            predicted_classes[this_class] = len(class_members["Objective Value"])
+            predicted_classes[this_class] = len(class_members["Class"])
 
     return predicted_classes.sort_values(ascending=False)
 
