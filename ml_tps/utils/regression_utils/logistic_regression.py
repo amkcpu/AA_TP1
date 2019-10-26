f4ef55a8230e59127e14c4645bab54d3afe30985
amkcpu <amkcpu@outlook.com>
2019-10-26 01:45:18 -0300

    [ADD] Tolerance parameter for LogisticRegression fitting

--- ml_tps/utils/regression_utils/logistic_regression.py
+++ ml_tps/utils/regression_utils/logistic_regression.py
@@ -11,22 +11,24 @@ class LogisticRegression:
         """:param initial_theta: Initial values for theta. Has to include bias term."""
         self.theta = initial_theta
 
-    def fit(self, X: pd.DataFrame, y: pd.Series, alpha: float=0.0001, iters: int=1000):
+    def fit(self, X: pd.DataFrame, y: pd.Series, alpha: float=0.0001, iters: int=1000, tol: float = 0.001):
         """Fit logistic regression parameters using gradient descent.
         
         :param X: Training set examples.
         :param y: Training set objective values.
         :param alpha: Specifies learning rate. Defaults to 0.0001.
         :param iters: Number of iterations of gradient descent algorithm. Defaults to 1000 iterations.
+        :param tol: Maximum error tolerance.
         """
         if self.theta is None:
             self.theta = pd.Series(np.zeros(len(X.columns) + 1))
 
         it = 0
-        while it < iters:
+        while it < iters and error > tol:
             theta_update = alpha * (X.T @ (sigmoid(X @ self.theta) - y))
             bias_update = alpha * sum(sigmoid(X @ self.theta) - y)
             self.theta -= bias_update.append(theta_update)
+            error = self.cost(X, y)
             it += 1
 
     def predict(self, X: pd.DataFrame):
