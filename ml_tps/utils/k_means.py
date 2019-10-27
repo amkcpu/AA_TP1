9d302c6dfa7971be65e35e3f6e0d70de227c1e4e
amkcpu <amkcpu@outlook.com>
2019-10-27 19:13:31 -0300

    [ADD] Finalize KMeans

--- ml_tps/utils/k_means.py
+++ ml_tps/utils/k_means.py
@@ -8,7 +8,28 @@ def initialize_centroids(X: pd.DataFrame, k: int):
     indexes = np.arange(len(X))
     np.random.shuffle(indexes)
     indexes = list(indexes)
-    centroids = pd.DataFrame([X.iloc[i] for i in indexes[:k]])
+    centroids = pd.DataFrame([X.iloc[i] for i in indexes[:k]], index=range(1, k+1))
+    return centroids
+
+
+def assign_centroids(X: pd.DataFrame, centroids: pd.DataFrame):
+    """Assigns the nearest centroid to each training example."""
+    X_assigned = X.copy()
+
+    for idx, row in X.iterrows():
+        distances = {i: euclidean_distance(row, r) for i, r in centroids.iterrows()}    # calc distance to each centroid
+        closest_centroid = min(distances, key=distances.get)    # assign training example to closest centroid
+        X_assigned.at[idx, "Centroid"] = closest_centroid
+
+    return X_assigned
+
+
+def move_centroids(X_assigned: pd.DataFrame, centroids: pd.DataFrame):
+    """Moves the centroids to the mean of their corresponding examples."""
+    for idx, row in centroids.iterrows():
+        assigned_rows = X_assigned[X_assigned["Centroid"] == idx]
+        centroids.at[idx, :] = assigned_rows.drop("Centroid", axis=1).mean()
+
     return centroids
 
 
@@ -17,20 +38,36 @@ class KMeans:
     def __init__(self, initial_centroids: pd.DataFrame = None):
         self.centroids = initial_centroids
 
-    def fit(self, X: pd.DataFrame, k: int, iters: int = 1000, tol: float = 0.001):
-        self.centroids = initialize_centroids(X, k)
+    def fit(self, X: pd.DataFrame, k: int, iters: int = 1000, tol: float = 0.001, initial_centroids: pd.DataFrame = None):
+        """Clusters a given data set into k clusters using the K-Means algorithm.
 
-        error = self.cost(X)
+        :param X: Data set on which to perform clustering.
+        :param k: Number of clusters to be found.
+        :param iters: Maximum number of iterations before clustering is stopped.
+        :param tol: Stop criteria for clustering. If the clustering error falls below the tolerance, clustering is stopped.
+        :param initial_centroids: If so wished, the starting centroids can be passed.
+        """
+        if initial_centroids is not None:
+            dim_cent = len(initial_centroids.columns)
+            dim_data = len(X.columns)
+            if not dim_cent == dim_data:
+                raise ValueError("initial_centroids have different dimension ({0}) than the data set ({1}).".format(dim_cent, dim_data))
+            self.centroids = initial_centroids
+        else:
+            self.centroids = initialize_centroids(X, k)
+
+        X_assigned = assign_centroids(X, self.centroids)
+        error = self.cost(X_assigned)
         it = 0
         while it < iters and error > tol:
-            centroids_prev = self.centroids
-            # TODO
-            #  calc distance of each training example to each centroid
-            #  assign each training example to its closest centroid
-            #  move centroid -> calculate mean of all its assigned examples
-            error = self.cost(X)
+            centroids_prev = self.centroids.copy()
+            self.centroids = move_centroids(X_assigned, self.centroids)
+            X_assigned = assign_centroids(X, self.centroids)
+
+            error = self.cost(X_assigned)
             it += 1
             if self.centroids.equals(centroids_prev):    # break if centroids are stable
+                print("Centroids have not been updated. K-Means finished.")
                 break
 
         print("Finished after {} iterations.".format(it))
@@ -40,19 +77,19 @@ class KMeans:
         if self.centroids is None:
             raise ValueError("Model has not been fitted yet (centroids = None).")
 
-        predictions = pd.Series(np.zeros(len(X)), index=X.index)
+        predictions = list()
         for idx, row in X.iterrows():
-            distances = pd.Series([euclidean_distance(centroid, row) for centroid in self.centroids],
-                                  index=self.centroids.index)
-            predictions[idx] = distances.sort_values(ascending=False).head(1).index
+            distances = {i: euclidean_distance(row, r) for i, r in self.centroids.iterrows()}
+            predictions.append(min(distances, key=distances.get))
 
-        return predictions
+        return pd.Series(predictions, index=X.index)
 
-    def cost(self, X) -> float:
-        n = len(X)
-        # TODO
+    def cost(self, X_assigned) -> float:
+        n = len(X_assigned)
+        costs = list()
+        for idx, row in self.centroids.iterrows():
+            assigned_rows = X_assigned[X_assigned["Centroid"] == idx].drop("Centroid", axis=1)
+            cost = sum([euclidean_distance(row, r) for i, r in assigned_rows.iterrows()]) / n
+            costs.append(cost)
 
-
-data = pd.DataFrame([[1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2]])
-clf = KMeans()
-a = 1
+        return sum(costs)
