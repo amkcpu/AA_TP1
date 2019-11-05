7dcd4e56f3ed6d27bc397c4501b7a3ee714e8271
amkcpu <amkcpu@outlook.com>
2019-11-05 17:10:05 -0300

    [ADD] Procedure comments for SOM

--- ml_tps/algorithms/self_organizing_map.py
+++ ml_tps/algorithms/self_organizing_map.py
@@ -1,4 +1,13 @@
+import pandas as pd
+
 # Self organizing Kohonen maps (SOM)
+# Algorithm
+# Initialize weights
+# for vector in all_example_vectors or until certain number of iterations N:
+#   Randomly choose weight vector
+#   Search for most similar weights using euclidean distance -> choose best matching unit (BMU)
+#   Update step: Update weights as well as the neighboring weights
+
 
 class SelfOrganizingMap:
 
