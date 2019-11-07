66ae529d4734b3ef4df05145e30d79373b572bce
amkcpu <amkcpu@outlook.com>
2019-11-06 22:47:35 -0300

    [MOD] Further KNN usability improvements

--- ml_tps/algorithms/k_nearest_neighbors.py
+++ ml_tps/algorithms/k_nearest_neighbors.py
@@ -14,8 +14,8 @@ class KNN:
         self.X_train = X_train
         self.y_train = y_train
 
-    def predict(self, examples: pd.DataFrame, k: int, weighted: bool = False):
-        """Predict class for a given example using the k-nearest neighbors algorithm.
+    def predict(self, examples: pd.DataFrame, k: int, weighted: bool = False) -> pd.Series:
+        """Predict class for given examples using the k-nearest neighbors algorithm.
 
         :param examples: Examples to be predicted.
         :param k: Amount of neighbors considered in KNN.
@@ -36,7 +36,7 @@ class KNN:
         return pd.Series(predictions)
 
 
-def get_nearest_neighbors(example: pd.Series, X_train: pd.DataFrame, y_train: pd.Series, k: int):
+def get_nearest_neighbors(example: pd.Series, X_train: pd.DataFrame, y_train: pd.Series, k: int) -> pd.DataFrame:
     example.index = X_train.columns
 
     distance = DistanceMetric("euclidean")
@@ -45,10 +45,11 @@ def get_nearest_neighbors(example: pd.Series, X_train: pd.DataFrame, y_train: pd
     neighbors = pd.concat([neighbors, y_train], axis=1)
     neighbors.columns = ["Distance", "Class"]
     neighbors = neighbors.sort_values(by="Distance", ascending=True)
+
     return neighbors.head(k)  # grab k nearest neighbors
 
 
-def prediction_per_class(nearest_neighbors: pd.DataFrame, weighted: bool):
+def prediction_per_class(nearest_neighbors: pd.DataFrame, weighted: bool) -> pd.Series:
     available_classes = nearest_neighbors["Class"].unique()  # which classes occur in nearest neighbors
     predicted_classes = pd.Series(np.array(np.zeros(len(available_classes))), index=available_classes)
 
@@ -62,7 +63,7 @@ def prediction_per_class(nearest_neighbors: pd.DataFrame, weighted: bool):
     return predicted_classes.sort_values(ascending=False)
 
 
-def choose_predict_class(predicted_classes: pd.Series):
+def choose_predict_class(predicted_classes: pd.Series) -> int:
     # Print error message if most frequently predicted classes have the same frequency
     if len(predicted_classes) > 1:
         if predicted_classes.iloc[0] == predicted_classes.iloc[1]:
